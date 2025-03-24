0c15e2194ada5460892d254aaf5cbe21616b8ca8
yoha-ni <yohan7913@naver.com>
2025-03-24 18:52:50 +0900

    #3 [Anonymous class, Lambda, Functional Interface] Functional Interface

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("net.datafaker:datafaker:2.4.2")
 }
 
 tasks.test {
---
+++ src/main/java/dev/yoha_ni/week4/day1/functionalinterface/thread/CallableExample.java
@@ -0,0 +1,23 @@
+package dev.yoha_ni.week4.day1.functionalinterface.thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableExample {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<String> task = () -> "result123";
+        Future<String> future = executor.submit(task);
+
+        try {
+            System.out.println("Callable Result = " + future.get());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        executor.shutdown();
+    }
+}
