83a9dcb2de58930c92eb922e558386185c68eb26
yoha-ni <yohan7913@naver.com>
2025-03-26 16:44:14 +0900

    #5 [Stream] Stream code

---
+++ src/main/java/dev/yoha_ni/week4/day3/stream/IntermediateOperation.java
@@ -0,0 +1,5 @@
+package dev.yoha_ni.week4.day3.stream;
+
+public class IntermediateOperation {
+
+}
---
+++ src/test/java/dev/yoha_ni/week4/day3/stream/EtcTest.java
@@ -0,0 +1,42 @@
+package dev.yoha_ni.week4.day3.stream;
+
+import net.datafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class EtcTest {
+    private final Faker faker = new Faker();
+
+    @Test
+    void streamCanNotBeReused() {
+        // given
+        Stream<String> names = Stream.generate(() -> faker.name().fullName())
+                .limit(3);
+        // when
+        names.forEach(System.out::println);
+        names.forEach(System.out::println);
+        // then
+    }
+
+    @Test
+    void streamNeedReused(){
+        List<String> names = Stream.generate(() -> faker.name().fullName())
+                .limit(2)
+                .toList();
+
+        names.stream().forEach(System.out::println);
+        names.stream().forEach(System.out::println);
+    }
+
+    @Test
+    void reduce(){
+        String pokemonName = Stream.generate(() -> faker.pokemon().name())
+                .limit(5)
+                .peek(System.out::println)
+                .reduce((String s1, String s2) -> s1 + "//" + s2)
+                .orElse("No Name");
+        System.out.println(pokemonName);
+    }
+}
