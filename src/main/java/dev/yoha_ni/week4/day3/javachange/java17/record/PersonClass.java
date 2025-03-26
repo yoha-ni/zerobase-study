36f60ea4e97538c9bbe9a57f3b92b2d478eb5aa8
yoha-ni <yohan7913@naver.com>
2025-03-26 21:53:30 +0900

    [Java version 의 변화] Class와 Record의 차이점

---
+++ src/main/java/dev/yoha_ni/week4/day3/javachange/java17/record/PersonClass.java
@@ -0,0 +1,19 @@
+package dev.yoha_ni.week4.day3.javachange.java17.record;
+
+public class PersonClass {
+    private final String name;
+    private final int age;
+
+    public PersonClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/dev/yoha_ni/week4/day3/javachange/java17/record/PersonRecord.java
@@ -0,0 +1,7 @@
+package dev.yoha_ni.week4.day3.javachange.java17.record;
+
+public record PersonRecord(
+        String name,
+        int age
+) {
+}
